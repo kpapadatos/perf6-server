73cba93d61382bc158760a737400d9ef1ef50010
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2021-09-14 11:15:03 +0300

    Fix

--- server/ClientRouter.ts
+++ server/ClientRouter.ts
@@ -1,11 +1,18 @@
 import { Router, static as serveStatic } from 'express';
+import { existsSync } from 'fs';
 import { resolve } from 'path';
+
+let indexFilePath = resolve(__dirname, '../dist/client/index.html');
+
+if (!existsSync(indexFilePath)) {
+    indexFilePath = resolve(__dirname, '../client/index.html');
+}
 
 export const ClientRouter = Router();
 
 ClientRouter.use(
     serveStatic('dist/client', { redirect: false }),
     (req, res) => {
-        res.sendFile(resolve(__dirname, '../dist/client/index.html'));
+        res.sendFile(indexFilePath);
     }
 );
